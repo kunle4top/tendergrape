b45331785c1071deb04f8f73408eca7601b6248e
Olakunke Peter <kunle4top@gmail.com>
2024-05-24 13:02:05 +0100

    Add files via upload

---
+++ js/run.js
@@ -0,0 +1,330 @@
+//CLASS NAVIGATION kg1
+function myFunction_11() {
+  var x = document.getElementById("kg11sterm");
+y= document.getElementById("kg12ndterm");
+n= document.getElementById("kg13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_21() {
+  var x = document.getElementById("kg12ndterm");
+  y= document.getElementById("kg11sterm");
+  n= document.getElementById("kg13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_31() {
+  var x = document.getElementById("kg13rdterm");
+  y= document.getElementById("kg11sterm");
+  n= document.getElementById("kg12ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+//CLASS NAVIGATION kg2
+function myFunction_41() {
+  var x = document.getElementById("kg21sterm");
+y= document.getElementById("kg22ndterm");
+n= document.getElementById("kg23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_51() {
+  var x = document.getElementById("kg22ndterm");
+  y= document.getElementById("kg21sterm");
+  n= document.getElementById("kg23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_61() {
+  var x = document.getElementById("kg23rdterm");
+  y= document.getElementById("kg21sterm");
+  n= document.getElementById("kg22ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+
+//CLASS NAVIGATION Nur 1
+function myFunction_71() {
+  var x = document.getElementById("nur11sterm");
+y= document.getElementById("nur12ndterm");
+n= document.getElementById("nur13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_81() {
+  var x = document.getElementById("nur12ndterm");
+  y= document.getElementById("nur11sterm");
+  n= document.getElementById("nur13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_91() {
+  var x = document.getElementById("nur13rdterm");
+  y= document.getElementById("nur11sterm");
+  n= document.getElementById("nur12ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+//CLASS NAVIGATION Nur 2
+function myFunction_101() {
+  var x = document.getElementById("nur21sterm");
+y= document.getElementById("nur22ndterm");
+n= document.getElementById("nur23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_201() {
+  var x = document.getElementById("nur22ndterm");
+  y= document.getElementById("nur21sterm");
+  n= document.getElementById("nur23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_301() {
+  var x = document.getElementById("nur23rdterm");
+  y= document.getElementById("nur21sterm");
+  n= document.getElementById("nur22ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+//CLASS NAVIGATION Pry 1
+function myFunction_401() {
+  var x = document.getElementById("pry11sterm");
+y= document.getElementById("pry12ndterm");
+n= document.getElementById("pry13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_501() {
+  var x = document.getElementById("pry12ndterm");
+  y= document.getElementById("pry11sterm");
+  n= document.getElementById("pry13rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_601() {
+  var x = document.getElementById("pry13rdterm");
+  y= document.getElementById("pry11sterm");
+  n= document.getElementById("pry12ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+
+//CLASS NAVIGATION Pry 2
+function myFunction_701() {
+  var x = document.getElementById("pry21sterm");
+y= document.getElementByd("pry22ndterm");
+n= document.getElementById("pry23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_801() {
+  var x = document.getElementById("pry22ndterm");
+  y= document.getElementById("pry21sterm");
+  n= document.getElementById("pry23rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_901() {
+  var x = document.getElementById("pry23rdterm");
+  y= document.getElementById("pry21sterm");
+  n= document.getElementById("pry22ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+//CLASS NAVIGATION Pry 3
+function myFunction_111() {
+  var x = document.getElementById("pry31sterm");
+y= document.getElementByd("pry32ndterm");
+n= document.getElementById("pry33rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_121() {
+  var x = document.getElementById("pry32ndterm");
+  y= document.getElementById("pry31sterm");
+  n= document.getElementById("pry33rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_131() {
+  var x = document.getElementById("pry33rdterm");
+  y= document.getElementById("pry31sterm");
+  n= document.getElementById("pry32ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
+
+
+//CLASS NAVIGATION Jss1 A
+function myFunction_201() {
+  var x = document.getElementById("js1a1sterm");
+y= document.getElementByd("js1a2ndterm");
+n= document.getElementById("js1a3rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+    n.style.display ="none";
+} else {
+    x.style.display = "none";
+  }
+}
+
+
+function myFunction_202() {
+  var x = document.getElementById("js1a2ndterm");
+  y= document.getElementById("js1a1sterm");
+  n= document.getElementById("js1a3rdterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";       
+  } else {
+    x.style.display = "none";
+    }
+}
+
+function myFunction_203() {
+  var x = document.getElementById("js1a3rdterm");
+  y= document.getElementById("js1a1sterm");
+  n= document.getElementById("js1a2ndterm");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display ="none";
+ n.style.display ="none";   
+  } else {
+    x.style.display = "none";
+    }
+}
